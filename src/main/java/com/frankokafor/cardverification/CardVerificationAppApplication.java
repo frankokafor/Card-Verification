5582f9d1732ef45cc5ccdf089709f86f059d03a1
frank okafor <frankokafor22@yahoo.com>
2019-12-09 12:44:55 +0100

    Project Started

---
+++ src/main/java/com/frankokafor/cardverification/CardVerificationAppApplication.java
@@ -0,0 +1,13 @@
+package com.frankokafor.cardverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardVerificationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardVerificationAppApplication.class, args);
+	}
+
+}
